3deaa16ec78c5cea35694a8d6dc3c7753428eefe
alirsadiq <72763457+alirsadiq@users.noreply.github.com>
2021-04-08 22:09:27 -0600

    first

---
+++ CSC3916_HW5-main/src/components/movie.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux'
+import { fetchMovie } from "../actions/movieActions";
+import MovieDetail from "../components/moviedetail"
+
+// support routing
+
+function Movie(props) {
+    const [selectedMovie] = useState(props.selectedMovie);
+    const params = useParams();
+    const title = params.title;
+    console.log(title);
+    const dispatch = useDispatch();
+    if (selectedMovie == null) {
+        dispatch(fetchMovie(title));
+    }
+
+    return (<MovieDetail movieTitle={title} />)
+}
+
+export default Movie;
